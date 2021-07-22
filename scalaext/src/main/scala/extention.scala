92c9a8c39c092d65722c0ebac50b8a1f05ae90c3
mahesh <mail.mahesh.lk@gmail.com>
2021-07-22 18:30:05 +0800

    scala-extention

--- scalaext/src/main/scala/extention.scala
+++ scalaext/src/main/scala/extention.scala
@@ -1,3 +1,13 @@
 
-@main def extention () =
+@main def extention () = {
   println("hello")
+  val us = Country("us",100)
+  extension (c: Country)
+    def topCity(city: String): String = c.name + city
+  println(us.topCity("NY"))
+
+  println(us)
+  println(us.topCity)
+}
+
+case class Country(name: String, positiveCases: Int)
