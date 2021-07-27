d6a0705b511bc27d0a1585926bfc2c714bb8f2ae
mahesh <mail.mahesh.lk@gmail.com>
2021-07-27 19:41:11 +0800

    own-map

--- ownmap/src/main/scala/Main.scala
+++ ownmap/src/main/scala/Main.scala
@@ -1,5 +1,13 @@
-@main def hello: Unit = 
-  println("Hello world!")
+@main def Kuztomizemap: Unit =
+  println("Kuztomize fn ")
   println(msg)
+  def double(i : Int) = i * 2
+  println(mapown(double, List(1, 2, 3)))
 
-def msg = "I was compiled by Scala 3. :)"
+  def strlen(s: String) = s.length
+  println(mapown(strlen, List("a", "bb", "ccc")))
+
+def msg = "Kuztomize Map function "
+
+def mapown[A, B](f: (B) => A, xs: List[B]): List[A] =
+  for x <- xs yield f(x)
