70fa22e7972f8ff297c5544403c1b1f96bc8312d
mahesh <mail.mahesh.lk@gmail.com>
2021-07-28 07:54:49 +0800

    own-map

--- ownmap/src/main/scala/Main.scala
+++ ownmap/src/main/scala/Main.scala
@@ -7,7 +7,14 @@
   def strlen(s: String) = s.length
   println(mapown(strlen, List("a", "bb", "ccc")))
 
+  val helloKuztomize = greet("function ")
+  //println(helloKuztomize)
+  println(helloKuztomize("Kuztomize"))
+
 def msg = "Kuztomize Map function "
 
 def mapown[A, B](f: (B) => A, xs: List[B]): List[A] =
   for x <- xs yield f(x)
+
+def greet(theGreeting: String): String => Unit =
+  (name: String) => println(s"$theGreeting, $name")
